775b436ecd57a10225876fd80a23c0d099c66e36
Rendy <rendyyanisusanto@gmail.com>
2025-09-16 20:29:46 +0700

    feat: Update metadata for registration page with enhanced descriptions and keywords

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,42 @@ const poppins = Poppins({
 
 export const metadata = {
   title: "Pendaftaran Santri Baru Asy-Syadzili",
-  description: "Sistem Pendaftaran Santri Baru Pondok Pesantren Asy-Syadzili",
+  description:
+    "Daftar santri baru Pondok Pesantren Asy-Syadzili secara online. Mudah, aman, dan langsung terhubung dengan admin. Dapatkan informasi pendaftaran, syarat, dan proses seleksi terbaru.",
+  keywords: [
+    "PPDB Asy-Syadzili",
+    "Pendaftaran Santri Baru",
+    "Pesantren Asy-Syadzili",
+    "PPDB Online",
+    "Santri Asy-Syadzili",
+    "Pondok Pesantren",
+    "Pendaftaran Pesantren",
+    "PPDB 2025"
+  ],
+  openGraph: {
+    title: "Pendaftaran Santri Baru Asy-Syadzili",
+    description:
+      "Daftar santri baru Pondok Pesantren Asy-Syadzili secara online. Mudah, aman, dan langsung terhubung dengan admin.",
+    url: "https://spmb-santripasir.vercel.app/", // Ganti dengan domain Anda jika sudah custom
+    siteName: "PPDB Asy-Syadzili",
+    images: [
+      {
+        url: "/1.png", // Pastikan logo sudah di folder public
+        width: 400,
+        height: 400,
+        alt: "Logo Asy-Syadzili"
+      }
+    ],
+    locale: "id_ID",
+    type: "website"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Pendaftaran Santri Baru Asy-Syadzili",
+    description:
+      "Daftar santri baru Pondok Pesantren Asy-Syadzili secara online. Mudah, aman, dan langsung terhubung dengan admin.",
+    images: ["/1.png"]
+  }
 };
 
 export default function RootLayout({ children }) {
